0b7983fb172b2ec7e8861c8f34b5fd12b8711ba3
Deusimar Moraes <deusimarmms@outlook.com>
2024-08-30 14:50:11 -0300

    first commit

---
+++ src/app/shared/form-busca/form-busca.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { ModalComponent } from '../modal/modal.component';
+import { MatDialog } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-form-busca',
+  templateUrl: './form-busca.component.html',
+  styleUrls: ['./form-busca.component.scss']
+})
+export class FormBuscaComponent {
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    this.dialog.open(ModalComponent);
+}
+}
